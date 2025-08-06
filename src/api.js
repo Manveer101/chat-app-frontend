93eab77c33926f08ff1eccb2d9ae30375d8e7ad9
manveer11011 <manveer11011@outlook.com>
2025-08-06 23:31:20 +0530

    done

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
- baseURL: import.meta.env.VITE_API_BASE_URL || "https://mychatapp-1-ooe6.onrender.com/api/",
+ baseURL: import.meta.env.VITE_API_BASE_URL || "https://r48hnsfc-8000.inc1.devtunnels.ms/api/",
 });
 
 api.interceptors.request.use((config) => {
